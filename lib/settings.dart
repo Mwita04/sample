ff5fe33c1fed0b88131ae8c4183fe8ce98fa3341
Mwita04 <emcee779@gmail.com>
2023-10-10 16:35:15 +0300

    tweak 2 with draggable bottom sheet

--- lib/settings.dart
+++ lib/settings.dart
@@ -1,6 +1,5 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class Settings extends StatelessWidget {
@@ -12,130 +11,33 @@ class Settings extends StatelessWidget {
       appBar: AppBar(
         title: Text('Settings'),
       ),
-      body: Center(
-        child: Container(
-          constraints: const BoxConstraints(maxWidth: 400),
-          child: ListView(
-            children: [
-              _SingleSection(
-                title: "General",
-                children: [
-                  const _CustomListTile(
-                      title: "About Phone",
-                      icon: CupertinoIcons.device_phone_portrait),
-                  _CustomListTile(
-                      title: "Dark Mode",
-                      icon: CupertinoIcons.moon,
-                      trailing:
-                          CupertinoSwitch(value: false, onChanged: (value) {})),
-                  const _CustomListTile(
-                      title: "System Apps Updater",
-                      icon: CupertinoIcons.cloud_download),
-                  const _CustomListTile(
-                      title: "Security Status",
-                      icon: CupertinoIcons.lock_shield),
-                ],
+      body: SizedBox.expand(
+          child: DraggableScrollableSheet(
+        initialChildSize: 0.5,
+        minChildSize: 0.2,
+        maxChildSize: 1.0,
+        builder: (BuildContext context, ScrollController scrollController) {
+          return Container(
+            decoration: const BoxDecoration(
+              color: Colors.tealAccent,
+              borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(48),
+                topRight: Radius.circular(48),
               ),
-              _SingleSection(
-                title: "Network",
-                children: [
-                  const _CustomListTile(
-                      title: "SIM Cards and Networks",
-                      icon: Icons.sd_card_outlined),
-                  _CustomListTile(
-                    title: "Wi-Fi",
-                    icon: CupertinoIcons.wifi,
-                    trailing: CupertinoSwitch(value: true, onChanged: (val) {}),
-                  ),
-                  _CustomListTile(
-                    title: "Bluetooth",
-                    icon: CupertinoIcons.bluetooth,
-                    trailing:
-                        CupertinoSwitch(value: false, onChanged: (val) {}),
-                  ),
-                  const _CustomListTile(
-                    title: "VPN",
-                    icon: CupertinoIcons.desktopcomputer,
-                  )
-                ],
-              ),
-              const _SingleSection(
-                title: "Privacy and Security",
-                children: [
-                  _CustomListTile(
-                      title: "Multiple Users", icon: CupertinoIcons.person_2),
-                  _CustomListTile(
-                      title: "Lock Screen", icon: CupertinoIcons.lock),
-                  _CustomListTile(
-                      title: "Display", icon: CupertinoIcons.brightness),
-                  _CustomListTile(
-                      title: "Sound and Vibration",
-                      icon: CupertinoIcons.speaker_2),
-                  _CustomListTile(
-                      title: "Themes", icon: CupertinoIcons.paintbrush)
-                ],
-              ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class _CustomListTile extends StatelessWidget {
-  final String title;
-  final IconData icon;
-  final Widget? trailing;
-  const _CustomListTile(
-      {Key? key, required this.title, required this.icon, this.trailing})
-      : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return ListTile(
-      title: Text(title),
-      leading: Icon(icon),
-      trailing: trailing ?? const Icon(CupertinoIcons.forward, size: 18),
-      onTap: () {},
-    );
-  }
-}
-
-class _SingleSection extends StatelessWidget {
-  final String title;
-  final List<Widget> children;
-  const _SingleSection({
-    Key? key,
-    required this.title,
-    required this.children,
-  }) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.start,
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        const SizedBox(height: 16),
-        Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: Text(
-            title.toUpperCase(),
-            style: Theme.of(context)
-                .textTheme
-                .displaySmall
-                ?.copyWith(fontSize: 16),
-          ),
-        ),
-        Container(
-          width: double.infinity,
-          color: Colors.white,
-          child: Column(
-            children: children,
-          ),
-        ),
-      ],
+            ),
+            child: ListView.builder(
+              controller: scrollController,
+              itemCount: 5,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text('Item $index'),
+                );
+              },
+            ),
+          );
+        },
+        expand: true,
+      )),
     );
   }
 }
