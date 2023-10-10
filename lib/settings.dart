e40ff3002ce2aa74a0d0b0019880dc78e2f2803b
Mwita04 <emcee779@gmail.com>
2023-10-10 17:41:10 +0300

    updated launcher icon.

--- lib/settings.dart
+++ lib/settings.dart
@@ -12,32 +12,33 @@ class Settings extends StatelessWidget {
         title: Text('Settings'),
       ),
       body: SizedBox.expand(
-          child: DraggableScrollableSheet(
-        initialChildSize: 0.5,
-        minChildSize: 0.2,
-        maxChildSize: 1.0,
-        builder: (BuildContext context, ScrollController scrollController) {
-          return Container(
-            decoration: const BoxDecoration(
-              color: Colors.tealAccent,
-              borderRadius: BorderRadius.only(
-                topLeft: Radius.circular(48),
-                topRight: Radius.circular(48),
+        child: DraggableScrollableSheet(
+          initialChildSize: 0.5,
+          minChildSize: 0.2,
+          maxChildSize: 1.0,
+          builder: (BuildContext context, ScrollController scrollController) {
+            return Container(
+              decoration: const BoxDecoration(
+                color: Colors.tealAccent,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(48),
+                  topRight: Radius.circular(48),
+                ),
               ),
-            ),
-            child: ListView.builder(
-              controller: scrollController,
-              itemCount: 5,
-              itemBuilder: (BuildContext context, int index) {
-                return ListTile(
-                  title: Text('Item $index'),
-                );
-              },
-            ),
-          );
-        },
-        expand: true,
-      )),
+              child: ListView.builder(
+                controller: scrollController,
+                itemCount: 5,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    title: Text('Item $index'),
+                  );
+                },
+              ),
+            );
+          },
+          expand: true,
+        ),
+      ),
     );
   }
 }
